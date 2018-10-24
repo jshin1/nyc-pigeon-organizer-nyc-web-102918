4d2faa4698c2537a914c805e313442c3166e8561
Ji S <j2yshin@gmail.com>
2018-10-24 20:40:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,6 +10,9 @@ def nyc_pigeon_organizer(data)
       v_array.each do |name|
         unless pigeon_list.has_key?(name)
           pigeon_list[name] = {}
+          if v_array.include?(name)
+            pigeon_list[name][attribute] << k.to_s
+          end
         end
       end
     end

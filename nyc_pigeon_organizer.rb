8dfce7175e53055c84e3aa5124e3a93c86f2c264
Ji S <j2yshin@gmail.com>
2018-10-24 19:23:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,11 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   
+  pigeon_list = {}
+  
   data.each do |attribute, attribute_data|
     attribute_data.each do |k, v_array|
-      binding.pry
-      k
+      
     end
   end
 end

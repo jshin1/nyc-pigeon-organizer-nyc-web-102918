21abfbe201b782140ef926d6ffe83ae44370c7fb
Ji S <j2yshin@gmail.com>
2018-10-24 21:06:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,15 +13,12 @@ def nyc_pigeon_organizer(data)
         end
         
         pigeon_list.each do |the_name, the_data|
-          binding.pry
-          pigeon_list[the_name][attribute]
+          pigeon_list[the_name][attribute] = []
+          if v_array.include?(the_name)
+            pigeon_list[the_name][attribute] << k.to_s
+          end
         end
-        
       end
-    end
-    pigeon_list[name][attribute] = []
-    if v_array.include?(name)
-      pigeon_list[name][attribute] << k.to_s
     end
   end
   pigeon_list

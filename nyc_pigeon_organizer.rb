85ce70486dd3ec00e51beccd0978e6802662e06b
Ji S <j2yshin@gmail.com>
2018-10-24 19:28:22 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, attribute_data|
     attribute_data.each do |k, v_array|
       v_array.each do |name|
-        unless pigeon_list.include?(name)
+        unless pigeon_list.has_key?()
         
     end
   end
